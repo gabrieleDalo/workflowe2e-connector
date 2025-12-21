ea13a622ed63e804ee14c7ee8572c41a5444623d
gabrieleDalo <gabriele.dalo@edu.unifi.it>
2025-12-21 12:38:24 +0100

    Changed time unti from ms to s

--- connector/workflowe2e/connector.go
+++ connector/workflowe2e/connector.go
@@ -1,293 +1,3 @@
-/*
-package workflowe2e
-
-import (
-	"context"
-	"errors"
-	"sort"
-	"time"
-
-	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/connector"
-	"go.opentelemetry.io/collector/consumer"
-	"go.opentelemetry.io/collector/pdata/pcommon"
-	"go.opentelemetry.io/collector/pdata/pmetric"
-	"go.opentelemetry.io/collector/pdata/ptrace"
-	"go.uber.org/zap"
-)
-
-var maxTimestamp = pcommon.Timestamp(^uint64(0))
-
-// connectorImp implements connector.Traces which means it must implement:
-// - component.Component (Start, Shutdown)
-// - consumer.Traces (ConsumeTraces)
-// Struttura del connector con i parametri desiderati
-type connectorImp struct {
-	metricsConsumer consumer.Metrics
-	cfg             *Config
-	logger          *zap.Logger
-
-	// Includi anche i seguenti parametri se non vuoi un'implementazione specifica per i metodi Start e Shutdown
-	//component.Start
-	//component.Shutdown
-}
-
-// createTracesToMetricsConnector della factory ha la firma corretta:
-// func(context.Context, connector.Settings, component.Config, consumer.Metrics) (connector.Traces, error)
-// Funzione per creare un nuovo connector
-func newConnector(
-	_ context.Context,
-	settings connector.Settings,
-	cfg component.Config,
-	metricsConsumer consumer.Metrics,
-) (connector.Traces, error) {
-
-	// type-assert la config al tuo tipo specifico
-	myCfg, ok := cfg.(*Config)
-	if !ok {
-		return nil, errors.New("invalid config, expected *workflowe2e.Config")
-	}
-
-	return &connectorImp{
-		metricsConsumer: metricsConsumer,
-		cfg:             myCfg,
-		logger:          settings.TelemetrySettings.Logger,
-	}, nil
-}
-
-// Definisce le capacità del connector, in particolare se modifica i dati ricevuti o no
-func (c *connectorImp) Capabilities() consumer.Capabilities {
-	return consumer.Capabilities{MutatesData: false}
-}
-
-// Assunzione: upstream è presente groupbytrace, quindi td contiene gli spans della stessa trace.
-func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
-
-	latencyMs, serviceActiveNs, err := c.calculateE2ELatency(td, c.cfg)
-	if err != nil {
-		return nil
-	}
-
-	md := pmetric.NewMetrics()               // Creazione del contenitore Metrics (root)
-	rm := md.ResourceMetrics().AppendEmpty() // Aggiunge un ResourceMetrics. Qui potresti aggiungere anche attributi di resource, es. rm.Resource().Attributes().PutStr("service.name", "workflow-e2e")
-	sm := rm.ScopeMetrics().AppendEmpty()    // Rappresenta la libreria di strumentazione. Di solito nei connector si lascia vuota
-
-	metric := sm.Metrics().AppendEmpty() // Qui definisci la metrica, con nome metrica → esposta a Prometheus e (opzionale) unit, description
-	metric.SetName(c.cfg.E2ELatencyMetricName)
-	metric.SetDescription("End-to-end workflow latency")
-	metric.SetUnit("ms")
-
-	// Espongo la latenza e2e come metrica, di tipo gauge o histogram
-	if c.cfg.EnableHistogram {
-		h := metric.SetEmptyHistogram()
-		dp := h.DataPoints().AppendEmpty()
-		dp.SetTimestamp(pcommon.Timestamp(time.Now().UnixNano()))
-
-		bounds := []float64{10, 50, 100, 500, 1000, 5000}
-		dp.ExplicitBounds().FromRaw(bounds)
-		dp.SetCount(1)
-		dp.SetSum(latencyMs)
-
-		counts := make([]uint64, len(bounds)+1)
-		for i, b := range bounds {
-			if latencyMs <= b {
-				counts[i] = 1
-				break
-			}
-		}
-		dp.BucketCounts().FromRaw(counts)
-
-	} else {
-		g := metric.SetEmptyGauge()
-		dp := g.DataPoints().AppendEmpty()
-		dp.SetDoubleValue(latencyMs)
-		dp.SetTimestamp(pcommon.Timestamp(time.Now().UnixNano()))
-	}
-
-	// Espongo la latenza per i singoli microservizi come metrica, di tipo gauge o histogram
-	if c.cfg.ServiceLatencyMode != "none" {
-
-		bounds := []float64{10, 50, 100, 500, 1000, 5000}
-
-		for svc, activeNs := range serviceActiveNs {
-
-			svcLatencyMs := float64(activeNs) / 1e6
-
-			m := sm.Metrics().AppendEmpty()
-			m.SetName(c.cfg.ServiceLatencyMetricName)
-			m.SetDescription("Per-service latency (active time)")
-			m.SetUnit("ms")
-
-			if c.cfg.EnableHistogram {
-				h := m.SetEmptyHistogram()
-				dp := h.DataPoints().AppendEmpty()
-				dp.SetTimestamp(pcommon.Timestamp(time.Now().UnixNano()))
-				dp.ExplicitBounds().FromRaw(bounds)
-				dp.SetCount(1)
-				dp.SetSum(svcLatencyMs)
-
-				counts := make([]uint64, len(bounds)+1)
-				for i, b := range bounds {
-					if svcLatencyMs <= b {
-						counts[i] = 1
-						break
-					}
-				}
-				dp.BucketCounts().FromRaw(counts)
-
-				// Aggiunge una label alla metrica, con il nome del servizio
-				dp.Attributes().PutStr("service-name", svc)
-			} else {
-				g := m.SetEmptyGauge()
-				dp := g.DataPoints().AppendEmpty()
-				dp.SetDoubleValue(svcLatencyMs)
-				dp.SetTimestamp(pcommon.Timestamp(time.Now().UnixNano()))
-				dp.Attributes().PutStr("service-name", svc)
-			}
-		}
-	}
-
-	// Invia le metriche (la struttura gerarchica creata (albero)) alla pipeline metrics
-	if err := c.metricsConsumer.ConsumeMetrics(ctx, md); err != nil {
-		if c.logger != nil {
-			c.logger.Error("failed to consume metrics", zap.Error(err))
-		}
-		return err
-	}
-
-	return nil
-}
-
-// interval usato per il merge degli spans di uno stesso servizio
-// Contiene il tempo di inizio e fine per uno span
-type interval struct {
-	start pcommon.Timestamp
-	end   pcommon.Timestamp
-}
-
-// Calcola la latenza end-to-end (ms) di una trace
-// Restituisce inoltre una mappa service -> durata attiva (ns)
-// NB: td è assunto come "trace completa" (groupbytrace upstream).
-func (c *connectorImp) calculateE2ELatency(td ptrace.Traces, cfg *Config) (float64, map[string]uint64, error) {
-
-	if td.ResourceSpans().Len() == 0 {
-		return 0, nil, errors.New("no spans")
-	}
-
-	var minStart pcommon.Timestamp = maxTimestamp
-	var maxEnd pcommon.Timestamp = 0
-
-	serviceIntervals := make(map[string][]interval)
-	enableService := cfg.ServiceLatencyMode != "none"
-
-	allowListMode := cfg.ServiceLatencyMode == "list"
-	allowMap := make(map[string]struct{}, len(cfg.ServiceAllowList))
-	if allowListMode {
-		for _, s := range cfg.ServiceAllowList {
-			allowMap[s] = struct{}{}
-		}
-	}
-
-	for i := 0; i < td.ResourceSpans().Len(); i++ {
-		rs := td.ResourceSpans().At(i)
-		for j := 0; j < rs.ScopeSpans().Len(); j++ {
-			ss := rs.ScopeSpans().At(j)
-			spans := ss.Spans()
-			for k := 0; k < spans.Len(); k++ {
-				span := spans.At(k)
-				start := span.StartTimestamp()
-				end := span.EndTimestamp()
-
-				if end == 0 {
-					continue
-				}
-
-				if start < minStart {
-					minStart = start
-				}
-				if end > maxEnd {
-					maxEnd = end
-				}
-
-				if !enableService {
-					continue
-				}
-
-				var svc string
-				if cfg.UsingIstio {
-					if v, ok := span.Attributes().Get(cfg.ServiceNameAttribute); ok {
-						svc = v.AsString()
-					}
-				}
-				if svc == "" {
-					if v, ok := rs.Resource().Attributes().Get(cfg.ServiceNameAttribute); ok {
-						svc = v.AsString()
-					}
-				}
-				if svc == "" {
-					svc = "unknown"
-				}
-
-				if allowListMode {
-					if _, ok := allowMap[svc]; !ok {
-						continue
-					}
-				}
-
-				// Alla fine del loop, per ogni servizio avrò tutti gli intervalli temporali dei suoi spans
-				serviceIntervals[svc] = append(serviceIntervals[svc], interval{start, end})
-			}
-		}
-	}
-
-	serviceActiveNs := make(map[string]uint64)
-	// Per ogni servizio, fa il merge degli intervalli per calcolare la latenza
-	for svc, ivs := range serviceIntervals {
-
-		// Ordina gli intervalli di uno span sulla base dell'istante di inizio
-		sort.Slice(ivs, func(i, j int) bool {
-			return ivs[i].start < ivs[j].start
-		})
-
-		cur := ivs[0] // Consideriamo il primo intervallo
-		var total uint64
-
-		// Loop per il merge, prende ogni intervallo successivo e lo confronti con cur
-		for i := 1; i < len(ivs); i++ {
-			// Controlla se l'intervallo inizia mentre uno è ancora attivo (lo span non è terminato)
-			if ivs[i].start <= cur.end {
-				if ivs[i].end > cur.end { // Se lo è, controlla se finisce dopo
-					cur.end = ivs[i].end // In tal caso aggiorno il tempo di fine
-				}
-			} else { // Se no, salvo il tempo trascorso in attività e passo al nuovo intervallo
-				total += uint64(cur.end - cur.start)
-				cur = ivs[i]
-			}
-		}
-		total += uint64(cur.end - cur.start)
-
-		serviceActiveNs[svc] = total
-	}
-
-	if minStart == maxTimestamp || maxEnd == 0 {
-		return 0, nil, errors.New("invalid timestamps")
-	}
-
-	latencyMs := float64(maxEnd-minStart) / 1e6
-	return latencyMs, serviceActiveNs, nil
-}
-
-// Opzionale, ridefinizione del metodo per avviare il connector
-func (c *connectorImp) Start(ctx context.Context, host component.Host) error {
-	return nil
-}
-
-// Opzionale, ridefinizione del metodo per chiudere il connector
-func (c *connectorImp) Shutdown(ctx context.Context) error {
-	return nil
-}
-*/
-
 package workflowe2e
 
 import (
@@ -366,13 +76,11 @@ func (c *connectorImp) Capabilities() consumer.Capabilities {
 // Assunzione: upstream è presente groupbytrace, quindi td contiene gli spans della stessa trace.
 func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
 
-	c.logger.Debug("PROVA: ConsumeTraces called",
-		zap.String("marker", "PROVA"),
+	c.logger.Debug("DEBUG_LOGS: ConsumeTraces called",
 		zap.Int("resource_spans_len", td.ResourceSpans().Len()),
 	)
 
-	c.logger.Debug("PROVA: ConsumeTraces called",
-		zap.String("marker", "PROVA"),
+	c.logger.Debug("DEBUG_LOGS: ConsumeTraces called",
 		zap.Int("span_count", td.SpanCount()),
 	)
 
@@ -393,8 +101,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 				}
 			}
 		}
-		c.logger.Debug("PROVA E2E latency computed",
-			zap.String("marker", "PROVA"),
+		c.logger.Debug("DEBUG_LOGS: E2E latency computed",
 			zap.String("trace_id", traceID),
 			zap.Float64("latency_ms", latencyMs),
 			zap.Int("num_services", len(serviceActiveNs)),
@@ -411,10 +118,10 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 	metric := sm.Metrics().AppendEmpty() // Qui definisci la metrica, con nome metrica → esposta a Prometheus e (opzionale) unit, description
 	metric.SetName(c.cfg.E2ELatencyMetricName)
 	metric.SetDescription("End-to-end workflow latency")
-	metric.SetUnit("ms")
+	metric.SetUnit("s")
 
-	// Buckets bounds (ms)
-	bounds := []float64{2, 4, 6, 8, 10, 50, 100, 200, 400, 800, 1000, 1400, 2000, 5000, 10_000, 15_000}
+	// Buckets bounds (s)
+	bounds := []float64{0.002, 0.004, 0.006, 0.008, 0.01, 0.05, 0.1, 0.2, 0.4, 0.8, 1.0, 1.4, 2.0, 5.0, 10.0, 15.0}
 
 	// Key fissa per la latenza E2E
 	const e2eKey = "__e2e__"
@@ -429,7 +136,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 		hs := c.getHistogramState(e2eKey, len(bounds)+1)
 		hs.mu.Lock()
 		count := hs.count
-		sumMs := float64(hs.sumNs) / 1e6
+		sumSec := float64(hs.sumNs) / 1e9
 		buckets := append([]uint64(nil), hs.buckets...)
 		hs.mu.Unlock()
 
@@ -439,7 +146,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 
 		dp.ExplicitBounds().FromRaw(bounds)
 		dp.SetCount(count)
-		dp.SetSum(sumMs)
+		dp.SetSum(sumSec)
 		dp.BucketCounts().FromRaw(buckets)
 
 		dp.Attributes().PutStr("service_name", e2eKey)
@@ -447,7 +154,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 	} else {
 		g := metric.SetEmptyGauge()
 		dp := g.DataPoints().AppendEmpty()
-		dp.SetDoubleValue(latencyMs)
+		dp.SetDoubleValue(latencyMs / 1000.0)
 		dp.SetTimestamp(pcommon.Timestamp(time.Now().UnixNano()))
 		dp.Attributes().PutStr("service_name", e2eKey)
 	}
@@ -462,7 +169,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 			m := sm.Metrics().AppendEmpty()
 			m.SetName(c.cfg.ServiceLatencyMetricName)
 			m.SetDescription("Per-service latency (active time)")
-			m.SetUnit("ms")
+			m.SetUnit("s")
 
 			if c.cfg.EnableHistogram {
 
@@ -475,7 +182,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 				hs := c.getHistogramState(key, len(bounds)+1)
 				hs.mu.Lock()
 				count := hs.count
-				sumMs := float64(hs.sumNs) / 1e6
+				sumSec := float64(hs.sumNs) / 1e9
 				buckets := append([]uint64(nil), hs.buckets...)
 				hs.mu.Unlock()
 
@@ -485,7 +192,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 
 				dp.ExplicitBounds().FromRaw(bounds)
 				dp.SetCount(count)
-				dp.SetSum(sumMs)
+				dp.SetSum(sumSec)
 				dp.BucketCounts().FromRaw(buckets)
 
 				// Aggiunge una label alla metrica, con il nome del servizio
@@ -494,7 +201,7 @@ func (c *connectorImp) ConsumeTraces(ctx context.Context, td ptrace.Traces) erro
 			} else {
 				g := m.SetEmptyGauge()
 				dp := g.DataPoints().AppendEmpty()
-				dp.SetDoubleValue(svcLatencyMs)
+				dp.SetDoubleValue(svcLatencyMs / 1000.0)
 				dp.SetTimestamp(pcommon.Timestamp(time.Now().UnixNano()))
 				dp.Attributes().PutStr("service_name", svc)
 			}
@@ -552,8 +259,7 @@ func (c *connectorImp) calculateE2ELatency(td ptrace.Traces, cfg *Config) (float
 				start := span.StartTimestamp()
 				end := span.EndTimestamp()
 
-				c.logger.Debug("PROVA span seen",
-					zap.String("marker", "PROVA"),
+				c.logger.Debug("DEBUG_LOGS: span seen",
 					zap.String("trace_id", span.TraceID().String()),
 					zap.String("span_name", span.Name()),
 					zap.Int64("start_ns", int64(start)),
@@ -637,8 +343,7 @@ func (c *connectorImp) calculateE2ELatency(td ptrace.Traces, cfg *Config) (float
 
 	latencyMs := float64(maxEnd-minStart) / 1e6
 
-	c.logger.Debug("PROVA trace times summary",
-		zap.String("marker", "PROVA"),
+	c.logger.Debug("DEBUG_LOGS: trace times summary",
 		zap.String("minStart_time", time.Unix(0, int64(minStart)).Format(time.RFC3339Nano)),
 		zap.String("maxEnd_time", time.Unix(0, int64(maxEnd)).Format(time.RFC3339Nano)),
 		zap.Float64("latency_ms_calc", latencyMs),
@@ -689,19 +394,18 @@ func (c *connectorImp) updateHistogram(key string, latencyNs uint64, bounds []fl
 	hs.count++
 	hs.sumNs += latencyNs
 
-	latencyMs := float64(latencyNs) / 1e6
+	latencySec := float64(latencyNs) / 1e9
 	idx := len(bounds) // +Inf bucket
 	// Controlla in quale bucket inserire la latenza
 	for i, b := range bounds { // NB: OTLP/Prometheus cumulano automaticamente i buckets poi
-		if latencyMs <= b {
+		if latencySec <= b {
 			idx = i
 			break
 		}
 	}
 	hs.buckets[idx]++
 
-	c.logger.Debug("PROVA histogram update",
-		zap.String("marker", "PROVA"),
+	c.logger.Debug("DEBUG_LOGS: histogram update",
 		zap.String("key", key),
 		zap.Uint64("count", hs.count),
 		zap.Uint64("sum_ns", hs.sumNs),
@@ -712,7 +416,7 @@ func (c *connectorImp) updateHistogram(key string, latencyNs uint64, bounds []fl
 // Opzionale, ridefinizione del metodo per avviare il connector
 func (c *connectorImp) Start(ctx context.Context, host component.Host) error {
 	if c.logger != nil {
-		c.logger.Info("PROVA connector starting",
+		c.logger.Info("DEBUG_LOGS: connector starting",
 			zap.String("metric_name_e2e", c.cfg.E2ELatencyMetricName), // se hai una Version nel cfg
 		)
 	}
